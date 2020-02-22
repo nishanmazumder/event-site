bfc9e1b5d1be77fbb862777e4fc35a121516fc78
Nishan Mozumder <arosh019@gmail.com>
2020-02-22 16:29:49 +0600

    inc/js/model

---
+++ inc/footer.php
@@ -0,0 +1,58 @@
+<!--Footer section strat-->
+<!---<footer class="fixed-bottom">
+        <div class="container">
+                <div class="row no-gutters">
+                        <div class="col-lg-8 col-lg-8 col-sm-8 footer-text">
+                                <nav aria-label="Page navigation example">
+                                  <ul class="pagination justify-content-end">
+                                        <li class="page-item">
+                                          <a class="page-link" href="#" aria-label="Previous">
+                                                <i class="fas fa-chevron-left"></i>
+                                          </a>
+                                        </li>
+                                        <li class="page-item"><a class="page-link" href="#">7</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">6</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">5</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">4</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                        <li class="page-item">
+                                          <a class="page-link" href="#" aria-label="Next">
+                                                <i class="fas fa-chevron-right"></i>
+                                          </a>
+                                        </li>
+                                  </ul>
+                                </nav>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4 float-right">
+                                <div class="nm_item_count">
+                                        <p class="text-secondary">Show</p>
+                                        <select class="nm_item_pagina" name="state">
+                                          <option value="blank">50</option>
+                                          <option value="WY">100</option>
+                                          <option value="WY">150</option>
+                                          <option value="AL">200</option>
+                                          <option value="WY">250</option>
+                                        </select>
+                                </div>
+                        </div>
+                </div>
+        </div>
+</footer>--->
+<footer class="">
+    <div class="container-fluid nm_dark">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="nm-txt-white text-center nm_mar_bott_null">All right reserved & copyright by Nishan</p>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--Footer section end-->
+
+<!--JS-->
+<?php include("scripts/js.php"); ?>
+
+</body>
+</html>
